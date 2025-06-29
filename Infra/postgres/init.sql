e37dff19b6f7b2eb61ec7200e6a3060493d6ee69
ZIDANE Abdellatif <zidane@et.esiea.fr>
2025-06-29 13:12:27 +0200

    data processing + database saving (complete mode)

---
+++ Infra/postgres/init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS nutriscore_counts (
+  nutriscore VARCHAR(10) PRIMARY KEY,
+  product_count INTEGER
+);
--- src/main/scala/com/esgi/Consumer/Consumer.scala
+++ src/main/scala/com/esgi/Consumer/Consumer.scala
@@ -4,6 +4,8 @@ import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.StreamingQuery
 import org.apache.spark.sql.types._
+import java.util.Properties
+
 
 object ConsumerKafka {
   // Define schema for the complete API response
@@ -18,45 +20,49 @@ object ConsumerKafka {
     ))
 
   def main(args: Array[String]): Unit = {
-    val bootstrap = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
-    val checkpoint = sys.env.getOrElse("CHECKPOINT_PATH", "checkpoint/generic")
+  val bootstrap = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+  val checkpoint = sys.env.getOrElse("CHECKPOINT_PATH", "checkpoint/generic")
 
-    val spark = SparkSession.builder()
-      .appName("ConsumerKafka")
-      .master("local[*]")
-      .config("spark.sql.shuffle.partitions", "5")
-      .getOrCreate()
+  val spark = SparkSession.builder()
+    .appName("ConsumerKafka")
+    .master("local[*]")
+    .config("spark.sql.shuffle.partitions", "5")
+    .getOrCreate()
 
-    import spark.implicits._
-    spark.sparkContext.setLogLevel("WARN")
+  import spark.implicits._
+  spark.sparkContext.setLogLevel("WARN")
 
-    // Read from Kafka
-    val rawStream = spark.readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", bootstrap)
-      .option("subscribe", "openfood")
-      .option("startingOffsets", "earliest")
-      .option("failOnDataLoss", "false")
-      .load()
+  // Read from Kafka
+  val rawStream = spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", bootstrap)
+    .option("subscribe", "openfood")
+    .option("startingOffsets", "earliest")
+    .option("failOnDataLoss", "false")
+    .load()
 
-    // Parse JSON and apply transformations
-    val transformedStream = rawStream
-      .select(from_json(col("value").cast("string"), apiResponseSchema).as("data"))
-      .select(explode(col("data.rows")).as("row"))
-      .select("row.row.*")
-      .transform(applyTransformations)
+  // Transform data
+  val transformedStream = rawStream
+    .select(from_json(col("value").cast("string"), apiResponseSchema).as("data"))
+    .select(explode(col("data.rows")).as("row"))
+    .select("row.row.*")
+    .transform(applyTransformations)
 
-    // Write to console
-    val query = transformedStream.writeStream
-      .format("console")
-      .outputMode("update")
-      .option("truncate", "false")
-      .option("numRows", "1000")
-      .option("checkpointLocation", checkpoint)
-      .start()
+  // Write to both console and PostgreSQL
+  val query = transformedStream.writeStream
+    .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
+      println(s"=== Batch $batchId ===")
+      batchDF.show(1000, truncate = false) // Console output
 
-    query.awaitTermination()
-  }
+      // Write to PostgreSQL
+      writeToPostgres(batchDF)
+    }
+    .outputMode("complete")
+    .option("checkpointLocation", checkpoint)
+    .start()
+
+  query.awaitTermination()
+}
 
   def applyTransformations(df: DataFrame): DataFrame = {
   val spark = SparkSession.getActiveSession.get
@@ -72,5 +78,17 @@ object ConsumerKafka {
   transformed
     .groupBy("nutriscore")
     .agg(count("*").as("product_count"))
+}
+
+def writeToPostgres(df: DataFrame): Unit = {
+  val jdbcUrl = sys.env("PG_URL")
+  val dbProps = new Properties()
+  dbProps.setProperty("user", sys.env("PG_USER"))
+  dbProps.setProperty("password", sys.env("PG_PWD"))
+  dbProps.setProperty("driver", "org.postgresql.Driver")
+
+  df.write
+    .mode("overwrite") // ou "append" si tu veux cumuler les valeurs
+    .jdbc(jdbcUrl, "nutriscore_counts", dbProps)
 }
 }
